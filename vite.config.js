25fd9e52d92aa588b39b2409104a2fe9570cf47e
Pandekte <marczynski.tomasz@gmail.com>
2024-10-02 04:21:06 +0200

    Adding gh-pages Removing generateResume.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 
 export default defineConfig({
+  base: "/personal-page/",
   plugins: [react(), ViteYaml()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        resume: resolve(__dirname, "resume.html"),
       },
     },
   },
