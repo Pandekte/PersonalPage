5c35871ede9a5fe257839a58236b9b51cf730222
Pandekte <marczynski.tomasz@gmail.com>
2024-10-02 16:38:00 +0200

    Inline asset path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
+// src/components/Card.jsx
+import React from "react";
+
 export default function Card({ title, description, image, url }) {
   return (
     <li>
       <a href={url} target="_blank" rel="noopener noreferrer">
-        <div className="card" style={{ "--bg-img": `url(${image})` }}>
+        <div
+          className="card"
+          style={{
+            "--bg-img": `url(${import.meta.env.BASE_URL}${image})`,
+          }}
+        >
           <h4>{title}</h4>
           <p>{description}</p>
         </div>
